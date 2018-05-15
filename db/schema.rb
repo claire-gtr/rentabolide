3050484ebfa52f2f7355728ddc7a0d1dac251715
Oscar Maybon <oscar.maybon@gmail.com>
2018-05-15 17:08:13 +0200

    update show view schema form is working but no redirection

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_131240) do
+ActiveRecord::Schema.define(version: 2018_05_15_144533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "start_time"
-    t.string "end_time"
     t.bigint "car_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_time"
+    t.date "end_time"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
