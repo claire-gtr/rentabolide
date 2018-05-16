24f6e09b9c8a038a8753e3d815abe2ce169d9dc2
Thomas Morin <thomasmorin1002@yahoo.fr>
2018-05-16 12:14:18 +0200

    Map Initialized

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,6 +2,15 @@ class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @cars = Car.all
+
+    @cars = Car.where.not(latitude: nil, longitude: nil)
+
+    @markers = @cars.map do |car|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
