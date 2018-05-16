6472eb45701389180347dfa344028e7d9fcce7e5
Claire Gautier <claire.gautier@edhec.com>
2018-05-16 12:13:07 +0200

    bookings controller + datepicker

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,69 +66,6 @@ puts 'Finished!'
 
 
 
-
-puts 'Creating bookings...'
-
-bookings_attributes = [
-  {
-    user: kevin,
-    name: "La ratisseuse",
-    model: "Citroen BX",
-    description: "Jantes targa racing 15 pouces, rétros m3, hp oxygen 3 voies 170w source sony s2200 cdr ampli boa 800w + caisson accoustique 1000w jbl"
-  },
-  {
-    user: kevin,
-    name: "Le lézard",
-    model: "Peugeot 206",
-    description: "Néon cup, jantes atomik 17 pouces, son surround, portes papillon, prises d'air racing"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  },
-  {
-    user: kevin,
-    name: "La fusée de la galaxie",
-    model: "Renault r21",
-    description: "Ailerons de toit, paupières de phare et de pare brise, échappement quadri-sorties"
-  }
-]
-
-
-
-
 puts 'Creating bookings...'
 kevina = User.create!(email: "kevina@wanadoo.com", password: "kekedu38")
 cartest = Car.last
