4235de08bff7c59afd6cd87e6957e42da0630b1c
Claire Gautier <claire.gautier@edhec.com>
2018-05-16 14:36:01 +0200

    réglé pb de dates

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2018_05_16_092942) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.string "photo_url"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
