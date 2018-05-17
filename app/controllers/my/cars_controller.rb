df0368eac57dda06eaf6cb8280d354f722fe5f80
Claire Gautier <claire.gautier@edhec.com>
2018-05-17 14:52:08 +0200

    delet a car ok

--- app/controllers/my/cars_controller.rb
+++ app/controllers/my/cars_controller.rb
@@ -2,4 +2,10 @@ class My::CarsController < ApplicationController
   def index
     @cars = Car.where(user_id: current_user)
   end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to my_cars_path
+  end
 end
