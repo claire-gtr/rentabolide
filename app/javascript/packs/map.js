93b690ac1f6ccc4f6f5d845e4ea499e03c287044
Thomas Morin <thomasmorin1002@yahoo.fr>
2018-05-17 17:22:44 +0200

    link marker to car ok

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,6 +4,7 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
+
   map.addMarkers(markers);
   if (markers.length === 0) {
     map.setZoom(2);
@@ -14,3 +15,40 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     map.fitLatLngBounds(markers);
   }
 }
+
+
+// const mapElement = document.getElementById('map');
+
+// if (mapElement) {
+//   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+
+//   const contentString = '<h4><%= car.name %></h4>'+
+//                        '<h6><%= car.model %></h6>'+
+//                        '<h6><%= car.description %></h6>';
+
+//   const infowindow = map.InfoWindow({
+//     content: contentString
+//   });
+
+//   const active_markers = markers.addListener('click', function() {
+//   infowindow.open(map, marker);
+
+//   const markers = JSON.parse(mapElement.dataset.active_markers);
+//   map.addMarkers(active_markers);
+
+
+//   });
+
+
+// //////////////////////////////////////////////
+
+//   if (markers.length === 0) {
+//     map.setZoom(2);
+//   } else if (markers.length === 1) {
+//     map.setCenter(markers[0].lat, markers[0].lng);
+//     map.setZoom(14);
+//   } else {
+//     map.fitLatLngBounds(markers);
+//   }
+// }
+
